80e6b3bf119efd25cb8c48ab3a5a56fc207ed478
Julian Lannigan <julian.lannigan@gmail.com>
2020-12-01 15:09:45 -0500

    feat(2020): efficiency

--- 2020/day1/process-report.js
+++ 2020/day1/process-report.js
@@ -16,23 +16,27 @@ function algo(report = []) {
         for (const [midIndex, value2] of report.entries()) {
             iterationCount += 1;
 
-            if (duoResult === undefined && topIndex !== midIndex && value1 + value2 === 2020) {
+            const duoAddition = value1 + value2;
+
+            if (duoResult === undefined && topIndex !== midIndex && duoAddition === 2020) {
                 duoNumbers = [value1, value2];
                 duoResult = value1 * value2;
             }
 
-            for (const [botIndex, value3] of report.entries()) {
-                iterationCount += 1;
+            if (duoAddition < 2020) {
+                for (const [botIndex, value3] of report.entries()) {
+                    iterationCount += 1;
 
-                if (topIndex === midIndex && midIndex === botIndex) {
-                    continue;
-                }
+                    if (topIndex === midIndex && midIndex === botIndex) {
+                        continue;
+                    }
 
-                if (value1 + value2 + value3 === 2020) {
-                    triNumbers = [value1, value2, value3];
-                    triResult = value1 * value2 * value3;
+                    if (value1 + value2 + value3 === 2020) {
+                        triNumbers = [value1, value2, value3];
+                        triResult = value1 * value2 * value3;
 
-                    break;
+                        break;
+                    }
                 }
             }
 
@@ -57,16 +61,28 @@ function algo(report = []) {
         .split('\n')
         .map(Number);
         
-    const { duoNumbers, triNumbers, duoResult, triResult, iterationCount } = algo(reportNumbers);
+    const { duoNumbers, triNumbers, duoResult, triResult, iterationCount } = algo([].concat(reportNumbers).sort((a, b) => .5 - Math.random()));
+    const sortedResult = algo([].concat(reportNumbers).sort((a, b) => a - b));
     
-    console.log('DUO Result:');
+    console.log('DUO Random Sorted Result:');
     console.log(`    Numbers that equal 2020: ${JSON.stringify(duoNumbers)}`);
     console.log(`    Report answer: ${duoResult}`);
     console.log('');
-    console.log('TRI Result:');
+    console.log('TRI Random Sorted Result:');
     console.log(`    Numbers that equal 2020: ${JSON.stringify(triNumbers)}`);
     console.log(`    Report answer: ${triResult}`);
     console.log('');
     console.log(`Report Size: ${reportNumbers.length}`);
-    console.log(`Iterations: ${iterationCount}`);
+    console.log(`Random Sorted Iterations: ${iterationCount}`);
+    console.log('');
+    console.log('DUO Sorted Result:');
+    console.log(`    Numbers that equal 2020: ${JSON.stringify(sortedResult.duoNumbers)}`);
+    console.log(`    Report answer: ${sortedResult.duoResult}`);
+    console.log('');
+    console.log('TRI Sorted Result:');
+    console.log(`    Numbers that equal 2020: ${JSON.stringify(sortedResult.triNumbers)}`);
+    console.log(`    Report answer: ${sortedResult.triResult}`);
+    console.log('');
+    console.log(`Report Size: ${reportNumbers.length}`);
+    console.log(`Sorted Iterations: ${sortedResult.iterationCount}`);
 })();
