b4d84cdf7ef8f85ad8f81ef4746764dcde583b2e
Julian Lannigan <julian.lannigan@gmail.com>
2021-12-11 23:01:50 +0000

    feat: day 3

--- 2021/README.md
+++ 2021/README.md
@@ -13,7 +13,7 @@
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
-![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
@@ -76,16 +76,16 @@ Both parts: 1.4633ms
 
 ```
 Day 02
-Time part 1: 0.863ms
-Time part 2: 1.403ms
-Both parts: 2.2656ms
+Time part 1: 0.825ms
+Time part 2: 0.91ms
+Both parts: 1.7351ms
 ```
 
 ```
 Day 03
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 1.937ms
+Time part 2: 0.839ms
+Both parts: 2.7757ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 4/50
-Total time: 3.729ms
+Total stars: 6/50
+Total time: 5.974ms
 ```
 
 <!--/RESULTS-->
---
+++ 2021/src/day03/index.ts
@@ -0,0 +1,138 @@
+import run from 'aocrunner';
+
+const parseInput = (rawInput: string) => rawInput.split('\n');
+
+type CountBitFrequencyResult = {
+  frequency: Map<number, number>,
+  filter: Map<number, string[]>
+}
+
+type CountBitOnlyFrequencyResult = Pick<CountBitFrequencyResult, 'frequency'>
+
+enum RatingType {
+  oxygen,
+  c02scrubber
+}
+
+
+export function countBitFreq(input: string[], bitPos: number, filter: true): CountBitFrequencyResult;
+export function countBitFreq(input: string[], bitPos: number, filter?: false): CountBitOnlyFrequencyResult;
+export function countBitFreq(input: string[], bitPos: number, filter = false): CountBitFrequencyResult | CountBitOnlyFrequencyResult {
+  const resultMap: Map<number, number> = new Map();
+  const resultFilter: Map<number, string[]> = new Map();
+
+  for (const row of input) {
+    const bit = Number(row[bitPos]);
+
+    resultMap.set(bit, (resultMap.get(bit) ?? 0) + 1);
+
+    if (filter) {
+      const curFilter = resultFilter.get(bit) ?? [];
+      curFilter.push(row);
+      resultFilter.set(bit, curFilter);
+    }
+  }
+
+  return {
+    frequency: resultMap,
+    filter: resultFilter
+  };
+}
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  const colCount = input[0].length;
+  let gammaRate = 0;
+  let epsilonRate = 0;
+
+  for (let curColumn = 0; curColumn < colCount; curColumn += 1) {
+    const { frequency } = countBitFreq(input, curColumn);
+
+    const onBitCount = frequency.get(1) ?? 0;
+    const offBitCount = frequency.get(0) ?? 0;
+
+    // doesn't account for if counts are the same.
+
+    const shift = colCount - curColumn - 1;
+
+    if (onBitCount > offBitCount) {
+      gammaRate = gammaRate + (1 << shift);
+      epsilonRate = epsilonRate + (0 << shift);
+    } else {
+      // offBit < onBit
+      gammaRate = gammaRate + (0 << shift);
+      epsilonRate = epsilonRate + (1 << shift);
+    }
+  }
+
+  return gammaRate * epsilonRate;
+};
+
+const findRating = (input: string[], ratingType: RatingType, bitPos = 0): number => {
+  const { frequency, filter } = countBitFreq(input, bitPos, true);
+
+  const onBitCount = frequency.get(1) ?? 0;
+  const offBitCount = frequency.get(0) ?? 0;
+
+  let targetBit: number = 0;
+
+  if (onBitCount === offBitCount) {
+    targetBit = 1;
+  } else {
+    targetBit = (onBitCount > offBitCount ? 1 : 0);
+  }
+
+  if (ratingType === RatingType.c02scrubber) {
+    targetBit ^= 1;
+  }
+
+  const newInput = filter.get(targetBit) ?? [];
+
+  if (newInput.length === 1 || bitPos + 1 === input[0].length) {
+    return parseInt(newInput[0], 2);
+  }
+
+  return findRating(newInput, ratingType, bitPos + 1);
+}
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return findRating(input, RatingType.oxygen) * findRating(input, RatingType.c02scrubber);
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `
+          00100
+          11110
+          10110
+          10111
+          10101
+          01111
+          00111
+          11100
+          10000
+          11001
+          00010
+          01010
+        `,
+        expected: 198,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
